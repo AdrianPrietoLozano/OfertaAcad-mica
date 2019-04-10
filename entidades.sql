159ef75743da98736ab1e1278a22f3f2366a8e18
Usuario <you@example.com>
2019-04-10 13:45:58 -0500

    Base de datos y tablas creadas

--- entidades.sql
+++ entidades.sql
@@ -5,17 +5,44 @@ CREATE TABLE materia(
 	creditos SMALLINT NOT NULL
 );
 
+
 CREATE TABLE profesor(
 	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 	nombre VARCHAR(100) NOT NULL,
 	ses TINYINT UNSIGNED NOT NULL
 );
 
-CREATE TABLE perido(
+
+CREATE TABLE periodo(
 	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 	fecha_inicio DATE NOT NULL,
 	fecha_fin DATE NOT NULL
 );
 
-CREATE TABLE horario(
+
+CREATE TABLE instancia_materia(
+	nrc INT UNSIGNED PRIMARY KEY,
+	cupos SMALLINT,
+	disponible SMALLINT,
+	id_materia INT UNSIGNED NOT NULL,
+	id_profesor INT UNSIGNED NOT NULL,
+	id_periodo INT UNSIGNED NOT NULL,
+	FOREIGN KEY(id_materia) REFERENCES materia(id),
+	FOREIGN KEY(id_profesor) REFERENCES profesor(id),
+	FOREIGN KEY(id_periodo) REFERENCES periodo(id)
 );
+
+
+CREATE TABLE horario(
+	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	hora VARCHAR(20) NOT NULL,
+	ses TINYINT UNSIGNED NOT NULL,
+	dias VARCHAR(10) NOT NULL,
+	aula VARCHAR(10) NOT NULL,
+	edificio VARCHAR(10) NOT NULL,
+	nrc_instancia_materia INT UNSIGNED NOT NULL,
+	FOREIGN KEY(nrc_instancia_materia) REFERENCES instancia_materia(nrc)
+);
+
+
+GRANT ALL PRIVILEGES ON oferta_academica . * TO 'adrian'@'localhost';
